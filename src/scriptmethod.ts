e12f11b1387ccf1e0d71a56dea960dc2df046e3f
Ebbe Fuglsang <ebof@universal-robots.com>
2025-12-05 21:55:43 +0100

    feat: functions udated to Polyscope 10.11 / 5.24 patch version bumped

--- src/scriptmethod.ts
+++ src/scriptmethod.ts
@@ -6,7 +6,6 @@ import motionFunctions from './motionFunctions.json';
 import internalsFunctions from './internalsFunctions.json';
 import urmathFunctions from './urmathFunctions.json';
 import interfacesFunctions from './interfacesFunctions.json';
-import URG6Functions from './URG6Functions.json';
 import ioconfigurationFunctions from './ioconfigurationFunctions.json';
 import worldModelFunctinos from './worldModelFunctions.json'
 import ROS2Functions from './ROS2Functions.json'
@@ -252,7 +251,6 @@ export function createFunctions(): ScriptMethod[] {
     functionList = functionList.concat(urmathFunctions.map(jsMthd => new ScriptMethod(jsMthd)));
     functionList = functionList.concat(interfacesFunctions.map(jsMthd => new ScriptMethod(jsMthd)));
     functionList = functionList.concat(ioconfigurationFunctions.map(jsMthd => new ScriptMethod(jsMthd)));
-    functionList = functionList.concat(URG6Functions.map(jsMthd => new ScriptMethod(jsMthd)));
     functionList = functionList.concat(worldModelFunctinos.map(jsMthd => new ScriptMethod(jsMthd)));
     functionList = functionList.concat(ROS2Functions.map(jsMthd => new ScriptMethod(jsMthd)));
     functionList = functionList.concat(nodocFunctions.map(jsMthd => new ScriptMethod(jsMthd)));
